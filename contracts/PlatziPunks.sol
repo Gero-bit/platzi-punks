4722cd503149d2751acb1e39ee3bb232f2bb7d3a
Geronimo Pylypchuk <geronimopylypchuk1@gmail.com>
2021-11-28 14:53:26 -0300

    Tests del Contrato

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -6,10 +6,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Base64.sol";
 import "./PlatziPunksDNA.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
 
     using Counters for Counters.Counter;
+    using Strings for uint256;
 
     Counters.Counter private _idCounter;
     uint256 public maxSupply;
@@ -87,7 +89,7 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
         string memory jsonURI = Base64.encode(
             abi.encodePacked(
             '{ "name": "PlatziPunks #',
-            tokenId,
+            tokenId.toString(),
             '", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
             image,
             '"}'
